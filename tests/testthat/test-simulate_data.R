50bed4848127613433beae146fd446941d86f1d6
Zhehao Wang <65267049+wzhhaohao@users.noreply.github.com>
2025-12-28 22:09:48 +0800

    Delete tests directory

--- tests/testthat/test-simulate_data.R
+++
@@ -1,6 +0,0 @@
-test_that("generate_sigma returns correct dimensions", {
-  mat <- generate_sigma(n_vars = 5, mode = "low")
-  print(mat)
-  expect_equal(dim(mat), c(5, 5))
-  expect_true(is.matrix(mat))
-})
